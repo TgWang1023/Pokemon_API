6929754d6260fccdfdea0284909972ee4595cff2
Tiange Wang <wtgkevin@gmail.com>
2018-08-09 15:06:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public/dist/public'));
+
+app.listen(8000, function(){
+    console.log('Running in localhost on port 8000!')
+})
